3c4c9dafcdae4a850d96646af5dca60ae2b2f7e7
tbalint19 <tothbalint719@gmail.com>
2023-06-28 19:23:44 +0200

    prettier works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.ts",
-  ],
+  content: ["./index.html", "./src/**/*.ts"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
