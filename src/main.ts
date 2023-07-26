96a166c3096ab3f992e8baf2a85c4d1cb630822e
Peti1991 <fodor.peter.1991@gmail.com>
2023-07-26 02:07:44 +0200

    added gallery

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { z } from "zod";
 
 let image = document.getElementById("image") as HTMLImageElement;
 let button = document.getElementById("button") as HTMLButtonElement;
-let dateInput = document.getElementById("date") as HTMLInputElement;
+let dateFromInput = document.getElementById("dateFrom") as HTMLInputElement;
+let dateUntilInput = document.getElementById("dateUntil") as HTMLInputElement;
 let title = document.getElementById("title") as HTMLHeadingElement;
 let explanation = document.getElementById(
   "explanation"
 ) as HTMLParagraphElement;
+let gallery = document.getElementById("gallery") as HTMLDivElement
 
 const NasaResponseSchema = z.object({
   date: z.string(),
@@ -18,21 +20,16 @@ const NasaResponseSchema = z.object({
   service_version: z.string(),
   title: z.string(),
   url: z.string(),
-});
+}).array();
 
 type NasaData = z.infer<typeof NasaResponseSchema>;
 
-function render(data: NasaData) {
-  image.src = data.url;
-  title.innerHTML = data.title;
-  explanation.innerHTML = data.explanation;
-}
-
-async function load() {
+async function load():Promise<null> {
   let response = await http.get("https://api.nasa.gov/planetary/apod", {
     params: {
       api_key: "418yCFUIF0MX0Q90XzpHhT84h2iWxhksYsCUvvGW",
-      date: dateInput.value,
+      start_date: dateFromInput.value,
+      end_date: dateUntilInput.value,
     },
   });
   let data = response.data;
@@ -43,7 +40,33 @@ async function load() {
     console.log(result.error);
     return null;
   }
-  render(data);
+  addContent(data);
+  return null
+}
+
+function addContent(data: NasaData) {
+  let content = ""
+  for(let i = 0; i < data.length; i++){
+    content += "<img " + `id=img${i+1}` + " src='" + data[i].url + "'/>"
+  }
+  gallery.innerHTML = content
+
+  for(let i = 0; i < data.length; i++){
+    document.getElementById(`img${i+1}`)?.addEventListener("click", () => {changeContent(data, i)})
+  }
+
+  image.src = data[0].url
+  title.innerHTML = data[0].title
+  explanation.innerHTML = data[0].explanation;
+}
+
+function changeContent(data: NasaData, i:number) {
+  image.src = data[i].url
+  title.innerHTML = data[i].title
+  explanation.innerHTML = data[i].explanation;
 }
 
 button.addEventListener("click", load);
+
+
+
